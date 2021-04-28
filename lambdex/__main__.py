0a8cca832ac5fd69ade5409f531c0ad85cf87a39
John Sirois <john.sirois@gmail.com>
2021-04-27 22:05:38 -0700

    Improve CLI ergonomics.
    + Add `-V` / `--version` support.
    + Use a help formatter that prints default values.
    + Add support for `python -mlambdex`.
    + Fix lambdex when called with no arguments to print help.

---
+++ lambdex/__main__.py
@@ -0,0 +1,9 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import
+
+from lambdex.bin import lambdex
+
+if __name__ == "__main__":
+    lambdex.main()
