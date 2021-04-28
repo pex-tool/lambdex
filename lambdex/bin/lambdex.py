ce4cd29e81917ea6ee7f7da65298687eaa561265
John Sirois <john.sirois@gmail.com>
2021-04-27 23:13:51 -0600

    Use black and isort. (#15)

--- lambdex/bin/lambdex.py
+++ lambdex/bin/lambdex.py
@@ -5,8 +5,8 @@ from __future__ import absolute_import, print_function
 
 import argparse
 import contextlib
-import json
 import hashlib
+import json
 import os
 import pkgutil
 import sys
@@ -15,63 +15,65 @@ import zipfile
 from pex.pex_bootstrapper import bootstrap_pex_env
 
 try:
-  # PEX >= 1.6.0
-  from pex.third_party.pkg_resources import EntryPoint
+    # PEX >= 1.6.0
+    from pex.third_party.pkg_resources import EntryPoint
 except ImportError:
-  # PEX < 1.6.0 has an install requirement of setuptools which we leverage knowledge of.
-  from pkg_resources import EntryPoint
+    # PEX < 1.6.0 has an install requirement of setuptools which we leverage knowledge of.
+    from pkg_resources import EntryPoint
 
 
 def die(msg):
-  print(msg, file=sys.stderr)
-  sys.exit(1)
+    print(msg, file=sys.stderr)
+    sys.exit(1)
 
 
 class LambdexInfo(object):
-  @classmethod
-  def from_string(cls, blob):
-    return LambdexInfo(**json.loads(blob))
+    @classmethod
+    def from_string(cls, blob):
+        return LambdexInfo(**json.loads(blob))
 
-  def __init__(self, entry_point):
-    self.entry_point = entry_point
+    def __init__(self, entry_point):
+        self.entry_point = entry_point
 
-  def to_json(self):
-    return json.dumps({'entry_point': self.entry_point})
+    def to_json(self):
+        return json.dumps({"entry_point": self.entry_point})
 
 
 def _write_zip_content(zf, filename, content):
-  info = zipfile.ZipInfo(filename)
-  info.external_attr = 0o755 << 16
-  zf.writestr(info, content)
+    info = zipfile.ZipInfo(filename)
+    info.external_attr = 0o755 << 16
+    zf.writestr(info, content)
 
 
 def write_lambdex_handler(pex_zip, options):
-  if (options.script is not None and options.entry_point is not None) or (
-      options.script is None and options.entry_point is None):
-    die('Must specify one of -s/--script or -e/--entry-point but not both.')
+    if (options.script is not None and options.entry_point is not None) or (
+        options.script is None and options.entry_point is None
+    ):
+        die("Must specify one of -s/--script or -e/--entry-point but not both.")
 
-  script = None
-  if options.script is not None:
-    method = options.handler
-    script = os.path.basename(options.script)
-    filename_prefix, ext = os.path.splitext(script)
-    if ext != '.py':
-      die('--script must be a python file that ends with ".py"')
-    # TODO(wickman) Validate that there is a symbol w/in the file that
-    # matches the method using ast.parse
-    entry_point = '%s:%s' % (filename_prefix, method)
-  else:
-    entry_point = options.entry_point
+    script = None
+    if options.script is not None:
+        method = options.handler
+        script = os.path.basename(options.script)
+        filename_prefix, ext = os.path.splitext(script)
+        if ext != ".py":
+            die('--script must be a python file that ends with ".py"')
+        # TODO(wickman) Validate that there is a symbol w/in the file that
+        # matches the method using ast.parse
+        entry_point = "%s:%s" % (filename_prefix, method)
+    else:
+        entry_point = options.entry_point
 
-  lambdex_info = LambdexInfo(entry_point)
+    lambdex_info = LambdexInfo(entry_point)
 
-  with contextlib.closing(zipfile.ZipFile(pex_zip, 'a')) as zf:
-    if script is not None:
-      with open(os.path.realpath(options.script), 'rb') as fp:
-        _write_zip_content(zf, script, fp.read())
-    _write_zip_content(zf, 'LAMBDEX-INFO', lambdex_info.to_json())
-    _write_zip_content(
-        zf, 'lambdex_handler.py', pkgutil.get_data('lambdex.resources', 'lambdex_handler.py'))
+    with contextlib.closing(zipfile.ZipFile(pex_zip, "a")) as zf:
+        if script is not None:
+            with open(os.path.realpath(options.script), "rb") as fp:
+                _write_zip_content(zf, script, fp.read())
+        _write_zip_content(zf, "LAMBDEX-INFO", lambdex_info.to_json())
+        _write_zip_content(
+            zf, "lambdex_handler.py", pkgutil.get_data("lambdex.resources", "lambdex_handler.py")
+        )
 
 
 # lambdex build foo.pex
@@ -79,155 +81,162 @@ def write_lambdex_handler(pex_zip, options):
 #   [-s script.py]
 #   [-e pkg:symbol]
 def build_lambdex(args):
-  write_lambdex_handler(args.pex, args)
+    write_lambdex_handler(args.pex, args)
 
 
 def configure_build_command(parser):
-  parser = parser.add_parser('build', help='build a lambdex package')
-  parser.set_defaults(func=build_lambdex)
+    parser = parser.add_parser("build", help="build a lambdex package")
+    parser.set_defaults(func=build_lambdex)
 
-  parser.add_argument(
-      'pex',
-      metavar='PEXFILE',
-      help='The pex file to turn into a lambdex package.')
+    parser.add_argument(
+        "pex", metavar="PEXFILE", help="The pex file to turn into a lambdex package."
+    )
 
-  parser.add_argument(
-      '-s', '--script',
-      dest='script',
-      metavar='FILENAME',
-      default=None,
-      help='The script to include, if any.')
+    parser.add_argument(
+        "-s",
+        "--script",
+        dest="script",
+        metavar="FILENAME",
+        default=None,
+        help="The script to include, if any.",
+    )
 
-  parser.add_argument(
-      '-e', '--entry-point',
-      dest='entry_point',
-      metavar='PACKAGE:NAME',
-      default=None,
-      help='Set the entry point of the lambda function to this package:name tuple.')
+    parser.add_argument(
+        "-e",
+        "--entry-point",
+        dest="entry_point",
+        metavar="PACKAGE:NAME",
+        default=None,
+        help="Set the entry point of the lambda function to this package:name tuple.",
+    )
 
-  parser.add_argument(
-      '-H', '--script-handler',
-      dest='handler',
-      default='handler',
-      metavar='FUNCTION',
-      help='Invoke this function within the script.  Default: "handler"')
+    parser.add_argument(
+        "-H",
+        "--script-handler",
+        dest="handler",
+        default="handler",
+        metavar="FUNCTION",
+        help='Invoke this function within the script.  Default: "handler"',
+    )
 
 
 def load_json_blob(filename):
-  if filename == '-':
-    return json.loads(sys.stdin.read())
-  else:
-    with open(filename, 'rb') as fp:
-      return json.load(fp)
+    if filename == "-":
+        return json.loads(sys.stdin.read())
+    else:
+        with open(filename, "rb") as fp:
+            return json.load(fp)
 
 
 CHUNK_SIZE = 64 * 1024
 
 
 def hash_file(filename, hasher=hashlib.sha256):
-  hash = hasher()
-  with open(filename, 'rb') as fp:
-    for chunk in iter(lambda: fp.read(CHUNK_SIZE), b''):
-      hash.update(chunk)
-  return hash.hexdigest()
+    hash = hasher()
+    with open(filename, "rb") as fp:
+        for chunk in iter(lambda: fp.read(CHUNK_SIZE), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
 
 
 def unzip(filename, path):
-  os.makedirs(path + '~')
-  with contextlib.closing(zipfile.ZipFile(filename, 'r')) as zfp:
-    zfp.extractall(path=path + '~')
-  os.rename(path + '~', path)
+    os.makedirs(path + "~")
+    with contextlib.closing(zipfile.ZipFile(filename, "r")) as zfp:
+        zfp.extractall(path=path + "~")
+    os.rename(path + "~", path)
 
 
 @contextlib.contextmanager
 def cached_environment(root, pex_file):
-  if os.path.isdir(pex_file):
-    yield pex_file
-    return
+    if os.path.isdir(pex_file):
+        yield pex_file
+        return
 
-  sha = hash_file(pex_file)
-  target = os.path.join(os.path.normpath(os.path.expanduser(root)), sha)
+    sha = hash_file(pex_file)
+    target = os.path.join(os.path.normpath(os.path.expanduser(root)), sha)
 
-  if not os.path.exists(target):
-    unzip(pex_file, target)
+    if not os.path.exists(target):
+        unzip(pex_file, target)
 
-  yield target
+    yield target
 
 
 @contextlib.contextmanager
 def chdir(dirname):
-  cwd = os.getcwd()
-  os.chdir(dirname)
-  yield
-  os.chdir(cwd)
+    cwd = os.getcwd()
+    os.chdir(dirname)
+    yield
+    os.chdir(cwd)
 
 
 # lambdex test [context configuration options] foo.pex <foo.json
 def test_lambdex(args):
-  bootstrap_pex_env(args.pex)
+    bootstrap_pex_env(args.pex)
 
-  with cached_environment(args.root, args.pex) as target:
-    with open(os.path.join(target, 'LAMBDEX-INFO'), 'rb') as fp:
-      lambdex_info_blob = fp.read()
+    with cached_environment(args.root, args.pex) as target:
+        with open(os.path.join(target, "LAMBDEX-INFO"), "rb") as fp:
+            lambdex_info_blob = fp.read()
 
-    lambdex_info = LambdexInfo.from_string(lambdex_info_blob)
+        lambdex_info = LambdexInfo.from_string(lambdex_info_blob)
 
-    sys.path.append(target)
+        sys.path.append(target)
 
-    with chdir(target):
-      runner = EntryPoint.parse('run = %s' % lambdex_info.entry_point).resolve()
-      if args.empty:
-        runner({}, None)
-      else:
-        for filename in args.files:
-          runner(load_json_blob(filename), None)
+        with chdir(target):
+            runner = EntryPoint.parse("run = %s" % lambdex_info.entry_point).resolve()
+            if args.empty:
+                runner({}, None)
+            else:
+                for filename in args.files:
+                    runner(load_json_blob(filename), None)
 
 
 def configure_test_command(parser):
-  parser = parser.add_parser('test', help='test a lambdex package')
-  parser.set_defaults(func=test_lambdex)
+    parser = parser.add_parser("test", help="test a lambdex package")
+    parser.set_defaults(func=test_lambdex)
 
-  parser.add_argument(
-      'pex',
-      metavar='PEXFILE',
-      help='The pex file to turn into a lambdex package.')
+    parser.add_argument(
+        "pex", metavar="PEXFILE", help="The pex file to turn into a lambdex package."
+    )
 
-  parser.add_argument(
-      'files',
-      metavar='FILE',
-      nargs='*',
-      help='A list of input json blobs to send as events to the function.')
+    parser.add_argument(
+        "files",
+        metavar="FILE",
+        nargs="*",
+        help="A list of input json blobs to send as events to the function.",
+    )
 
-  parser.add_argument(
-      '--empty',
-      dest='empty',
-      default=False,
-      action='store_true',
-      help='If specified, use an empty test event rather than reading '
-           'events from the command line.')
+    parser.add_argument(
+        "--empty",
+        dest="empty",
+        default=False,
+        action="store_true",
+        help="If specified, use an empty test event rather than reading "
+        "events from the command line.",
+    )
 
-  parser.add_argument(
-      '--root',
-      dest='root',
-      default='~/.lambdex',
-      help='If specified, cache lambdex test environments here.')
+    parser.add_argument(
+        "--root",
+        dest="root",
+        default="~/.lambdex",
+        help="If specified, cache lambdex test environments here.",
+    )
 
 
 def configure_clp():
-  parser = argparse.ArgumentParser()
-  subparsers = parser.add_subparsers()
-  configure_build_command(subparsers)
-  configure_test_command(subparsers)
-  return parser
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    configure_build_command(subparsers)
+    configure_test_command(subparsers)
+    return parser
 
 
 def main(args=None):
-  args = args or sys.argv[1:]
+    args = args or sys.argv[1:]
 
-  parser = configure_clp()
-  args = parser.parse_args(args=args)
-  args.func(args)
+    parser = configure_clp()
+    args = parser.parse_args(args=args)
+    args.func(args)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
--- lambdex/resources/lambdex_handler.py
+++ lambdex/resources/lambdex_handler.py
@@ -5,45 +5,50 @@ import os
 import sys
 
 __entry_point__ = None
-if '__file__' in locals() and __file__ is not None:
-  __entry_point__ = os.path.dirname(__file__)
-elif '__loader__' in locals():
-  from pkgutil import ImpLoader
-  if hasattr(__loader__, 'archive'):
-    __entry_point__ = __loader__.archive
-  elif isinstance(__loader__, ImpLoader):
-    __entry_point__ = os.path.dirname(__loader__.get_filename())
+if "__file__" in locals() and __file__ is not None:
+    __entry_point__ = os.path.dirname(__file__)
+elif "__loader__" in locals():
+    from pkgutil import ImpLoader
+
+    if hasattr(__loader__, "archive"):
+        __entry_point__ = __loader__.archive
+    elif isinstance(__loader__, ImpLoader):
+        __entry_point__ = os.path.dirname(__loader__.get_filename())
 
 if __entry_point__ is None:
-  sys.stderr.write('Could not launch python executable!\\n')
-  sys.exit(2)
+    sys.stderr.write("Could not launch python executable!\\n")
+    sys.exit(2)
 
 sys.path[0] = os.path.abspath(sys.path[0])
-sys.path.insert(0, os.path.abspath(os.path.join(__entry_point__, '.bootstrap')))
+sys.path.insert(0, os.path.abspath(os.path.join(__entry_point__, ".bootstrap")))
 
 try:
-  # PEX >= 1.6.0
-  from pex.third_party.pkg_resources import EntryPoint as __EntryPoint
-  from pex.pex_bootstrapper import bootstrap_pex_env
+    # PEX >= 1.6.0
+    from pex.pex_bootstrapper import bootstrap_pex_env
+    from pex.third_party.pkg_resources import EntryPoint as __EntryPoint
 except ImportError:
-  # PEX < 1.6.0 has an install requirement of setuptools which we leverage knowledge of.
-  from pkg_resources import EntryPoint as __EntryPoint
-  from _pex.pex_bootstrapper import bootstrap_pex_env
+    # PEX < 1.6.0 has an install requirement of setuptools which we leverage knowledge of.
+    from _pex.pex_bootstrapper import bootstrap_pex_env
+    from pkg_resources import EntryPoint as __EntryPoint
 
 bootstrap_pex_env(__entry_point__)
 
 import zipfile
+
 if zipfile.is_zipfile(__entry_point__):
-  import contextlib
-  with contextlib.closing(zipfile.ZipFile(__entry_point__)) as zf:
-    __lambdex_info_blob = zf.read('LAMBDEX-INFO')
+    import contextlib
+
+    with contextlib.closing(zipfile.ZipFile(__entry_point__)) as zf:
+        __lambdex_info_blob = zf.read("LAMBDEX-INFO")
 else:
-  with open(os.path.join(__entry_point__, 'LAMBDEX-INFO'), 'rb') as fp:
-    __lambdex_info_blob = fp.read()
+    with open(os.path.join(__entry_point__, "LAMBDEX-INFO"), "rb") as fp:
+        __lambdex_info_blob = fp.read()
 
 import json as __json
+
 __lambdex_info = __json.loads(__lambdex_info_blob)
-__RUNNER = __EntryPoint.parse('run = %s' % __lambdex_info['entry_point']).resolve()
+__RUNNER = __EntryPoint.parse("run = %s" % __lambdex_info["entry_point"]).resolve()
+
 
 def handler(event, context):
-  return __RUNNER(event, context)
+    return __RUNNER(event, context)
