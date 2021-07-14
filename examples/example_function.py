6c9a119fd9855e63caa738fcef36ec8462a7bfbc
Jeremy Lewis <luislew@gmail.com>
2021-07-13 21:25:10 -0700

    Polish up runtime lambdex entry point support
    * Update `lambdex test` to respect `LAMBDEX_ENTRY_POINT`
    * Add `other_handler` to `examples/example_function.py`
    * `tox.ini` updates:
      * Update `pex` testenv to use the latest version of PEX (`2.1.43`)
      * Add `entry-point-env-var` testenv which uses `other_handler`
      * Add `tox -e entry-point-env-var` command to `_integration`

--- examples/example_function.py
+++ examples/example_function.py
@@ -6,3 +6,7 @@ import requests
 def handler(event, context):
     url = event["url"]
     print("%s sha256:%s" % (url, hashlib.sha256(requests.get(url).content).hexdigest()))
+
+
+def other_handler(event, context):
+    print("Other handler invoked")
