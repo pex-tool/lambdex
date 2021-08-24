550b64522fc0a89d2f34f49497bf98a18bfadb73
Chris Williams <me@williamschris.com>
2021-08-23 21:04:33 -0500

    add integration test to support gcp-http type

--- examples/gcp_http/example_http_function.py
+++ examples/gcp_http/example_http_function.py
@@ -6,9 +6,12 @@ import requests
 
 def handler(request: flask.Request):
     request_json = request.get_json()
-    url = request_json["url"]
-    print("%s sha256:%s" % (url, hashlib.sha256(requests.get(url).content).hexdigest()))
+    url = request_json.get("url")
+    if not url:
+        print("No URL provided!")
+        return {}
 
+    print("%s sha256:%s" % (url, hashlib.sha256(requests.get(url).content).hexdigest()))
     return {
         "url": flask.escape(url),
         "sha256": hashlib.sha256(requests.get(url).content).hexdigest(),
